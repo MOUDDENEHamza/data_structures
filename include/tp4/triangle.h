4d4755950668be3081b2210ebfb336807b397d20
hamza <hz.mouddene@gmail.com>
2019-02-09 22:05:59 +0100

    tp5 done

--- include/tp4/triangle.h
+++ include/tp4/triangle.h
@@ -5,5 +5,11 @@
 
 /*Determine the center of gravity*/
 Point center_gravity(Point *pt1, Point *pt2, Point *pt3);
-	
+
+/*Calculate the perimeter of the triangle by tree given points*/
+float perimeter_triangle(Point *pt1, Point *pt2, Point *pt3);
+
+/*Calculate the area of the triangle by tree given points*/
+float area_triangle(Point *pt1, Point *pt2, Point *pt3);
+
 #endif
--- src/tp4/triangle.c
+++ src/tp4/triangle.c
@@ -11,3 +11,25 @@ Point center_gravity(Point *pt1, Point *pt2, Point *pt3) {
 	center_point.z = -(plan.equation[0] * center_point.x + plan.equation[1] * center_point.y + plan.equation[3]) * (1 / plan.equation[2]);
 	return center_point;
 }
+
+/*Calculate the perimeter of the triangle by tree given points*/
+float perimeter_triangle(Point *pt1, Point *pt2, Point *pt3) {
+	float res;
+	Vector vect1, vect2, vect3;
+	vect1 = vectorize(pt1, pt2);
+	vect2 = vectorize(pt2, pt3);
+	vect3 = vectorize(pt1, pt3);
+	res = norm(&vect1) + norm(&vect2) + norm(&vect3);
+	return res;
+}
+
+/*Calculate the area of the triangle by tree given points*/
+float area_triangle(Point *pt1, Point *pt2, Point *pt3) {
+	float res;
+	Vector vect1, vect2, vect3;
+        vect1 = vectorize(pt1, pt2);
+        vect2 = vectorize(pt2, pt3);
+	vect3 = vector_product(&vect1, &vect2);
+	res = (nomr(&vect1) * norm(&vect3)) / 3;
+	return res;
+}
